83b4ea46607d264a13cec84b96de6e509ec16c37
Ubuntu <ubuntu@ip-172-31-5-254.us-east-2.compute.internal>
2021-12-31 10:33:05 +0000

    added additional bruteforce tests

--- src/Parity_test.go
+++ src/Parity_test.go
@@ -49,12 +49,33 @@ func simulateFailure(blocks []uint64, failedBlock uint64, parity uint64, t *test
 	}
 }
 
-func bruteForce(blocks []uint64, t *testing.T) {
+func bruteForceRecovery(blocks []uint64, t *testing.T) {
 	parity := ComputeParity(blocks)
 
 	for i := range blocks {
 		simulateFailure(blocks, uint64(i), parity, t)
 	}
+}
+
+func BruteForceSingleBlockUpdate(blockIndex uint64, blocks []uint64, startValue uint64, maxValue uint64, t *testing.T) {
+	incrementalParity := ComputeParity(blocks)
+
+	for i := uint64(startValue); i < maxValue; i++ {
+		oldBlock := blocks[blockIndex]
+		blocks[blockIndex] = i
+
+		incrementalParity = UpdateParity(oldBlock, blocks[blockIndex], incrementalParity)
+
+		parityComputedFromScratch := ComputeParity(blocks)
+		if incrementalParity != parityComputedFromScratch {
+			t.Error("Fresh computed parity", parityComputedFromScratch, "rolling parity", incrementalParity, "blocks", blocks)
+		}
+
+		for i := range blocks {
+			simulateFailure(blocks, uint64(i), incrementalParity, t)
+		}
+	}
+
 }
 
 // ===================================================================================================================
@@ -86,27 +107,56 @@ func TestRecoverLostBlock_simple(t *testing.T) {
 	}
 }
 
-func TestBruteForceCloseTo0(t *testing.T) {
+func TestBruteForceRecoveryCloseTo0(t *testing.T) {
 	const maxValue uint64 = 100
 
 	for x := uint64(0); x < maxValue; x++ {
 		for y := uint64(0); y < maxValue; y++ {
 			for z := uint64(0); z < maxValue; z++ {
-				bruteForce([]uint64{x, y, z}, t)
+				bruteForceRecovery([]uint64{x, y, z}, t)
 			}
 		}
 	}
 }
 
-func TestBruteForceCloseToMax(t *testing.T) {
+func TestBruteForceRecoveryCloseToMax(t *testing.T) {
 	const startValue uint64 = ^uint64(0) - 100
 	const maxValue uint64 = ^uint64(0)
 
 	for x := uint64(startValue); x < maxValue; x++ {
 		for y := uint64(startValue); y < maxValue; y++ {
 			for z := uint64(startValue); z < maxValue; z++ {
-				bruteForce([]uint64{x, y, z}, t)
+				bruteForceRecovery([]uint64{x, y, z}, t)
 			}
 		}
 	}
+}
+
+func TestBruteForceUpdates(t *testing.T) {
+	// In this test given a set of blocks and their parity:
+	// [Block0, Block1, Block2] Parity
+	//
+	// We try to change each block selectivelly, calculate the parity incrementally
+	// and verify that the incremental pairty is the same that we compute if we compute it
+	// from scratch
+
+	const block0 uint64 = 0
+	const block1 uint64 = 1
+	const block2 uint64 = 2
+
+	const startValue uint64 = 0
+	const maxValue uint64 = 10000
+
+	const startValueMax uint64 = ^uint64(0) - 10000
+	const maxValueMax uint64 = ^uint64(0)
+
+	// Bruteforce close to 0
+	BruteForceSingleBlockUpdate(block0, initialParityBlocks, startValue, maxValue, t)
+	BruteForceSingleBlockUpdate(block1, initialParityBlocks, startValue, maxValue, t)
+	BruteForceSingleBlockUpdate(block2, initialParityBlocks, startValue, maxValue, t)
+
+	// Bruteforce close to max uint64
+	BruteForceSingleBlockUpdate(block0, initialParityBlocks, startValueMax, maxValueMax, t)
+	BruteForceSingleBlockUpdate(block1, initialParityBlocks, startValueMax, maxValueMax, t)
+	BruteForceSingleBlockUpdate(block2, initialParityBlocks, startValueMax, maxValueMax, t)
 }
